1c6bddc8247d36dabcaf386f128ea3fb9ca19d44
Cameron Samuels <me@cameronsamuels.com>
2018-05-12 16:40:14 -0500

    (update)Updated the website (#24)
    * Root
    
    * Js
    
    * CSS
    
    * Delete 256.png
    
    * Delete cube-bg.png
    
    * Delete github.svg
    
    * Delete googolmod.svg
    
    * Delete icon.png
    
    * Delete spartan.png
    
    * Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var fidgetIcon = document.querySelector("body>div:nth-of-type(5) img");
-fidgetIcon.addEventListener("mousedown", function(e) { e.preventDefault() });
-fidgetIcon.addEventListener("touchstart", function(e) { e.preventDefault() });
-fidgetIcon.addEventListener("dragstart", function(e) { e.preventDefault() });
+var images = document.querySelectorAll("img");
+for (i = 0; i < images.length; i++) {
+  images[i].addEventListener("dragstart", function(e) { e.preventDefault() });
+}
 
-document.querySelector("body>div:nth-of-type(7) img").addEventListener("dragstart", function(e) { e.preventDefault() });
-
+document.querySelector("body>div:nth-of-type(3)").addEventListener("click", function() { location = "https://git.io/vQlve" });
 document.querySelector("body>div:nth-of-type(5)").addEventListener("click", function() { location = "https://git.io/vpZxN" });
 document.querySelector("body>div:nth-of-type(8)").addEventListener("click", function() { location = "https://bocracy.com" });
+document.querySelector("body>div:nth-of-type(11)").addEventListener("click", function() { location = "https://git.io/vQlve" });
