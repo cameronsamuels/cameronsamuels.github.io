f30352627aa17ee70af1287321c56a198f46a5b2
Cameron Samuels <me@cameronsamuels.com>
2018-09-21 17:36:30 -0500

    Complete Revamp

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   
   document.body.addEventListener("dragstart", function(e) { e.preventDefault() });
   
-  document.querySelector("body>div:nth-of-type(5)").addEventListener("click", function(){ window.location = "https://git.io/vpZxN" });
+  document.querySelector("main:nth-of-type(2)>div:nth-of-type(5)").addEventListener("click", function(){ window.location = "https://git.io/vpZxN" });
   
 });
