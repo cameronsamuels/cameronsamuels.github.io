f97e0a6b573cf1ffccdbc07eec49184f3018ea24
Cameron Samuels <me@cameronsamuels.com>
2018-10-02 19:28:58 -0500

    Deleted the javascript file

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
-  document.body.addEventListener("dragstart", function(e) { e.preventDefault() });
-  
-  document.querySelector("main:nth-of-type(2)>div:nth-of-type(5)").addEventListener("click", function(){ window.location = "https://git.io/vpZxN" });
-  
-});
